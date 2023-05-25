dc6945348222c657677f52b232f8576bdcb001a0
Ondrej Antos <little.joy3080@fastmail.com>
2023-05-25 23:42:24 +0300

    add keymap

---
+++ after/plugin/keymap.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "Q", "<nop>", { desc = "I don't like macros" } )
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move down and center cursor line" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Move up and center cursor line" })
