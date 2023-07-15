84755917384390823d0ef96855da33e8679a6f92
Ondrej Antos <little.joy3080@fastmail.com>
2023-07-15 13:52:14 +0300

    add keymaps for telescope

--- after/plugin/keymap.lua
+++ after/plugin/keymap.lua
@@ -2,3 +2,9 @@ vim.keymap.set("n", "Q", "<nop>", { desc = "I don't like macros" } )
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move down and center cursor line" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Move up and center cursor line" })
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Telescope find buffers" })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "Telescope help tags" })
