5b7f1b5907cb8ea7f6c62281cc74d7a382604851
Ondrej Antos <ondrej.antos@innokasmedical.fi>
2023-08-04 09:07:52 +0300

    move keymap and options to the right place

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -3,6 +3,9 @@ vim.keymap.set("n", "Q", "<nop>", { desc = "I don't like macros" })
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move down and center cursor line" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Move up and center cursor line" })
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Telescope find files" })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Telescope live grep" })
